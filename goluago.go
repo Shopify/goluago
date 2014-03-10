9cdd2bae74534668c45e561bd266a2518bcf9c3c
Antoine Grondin <antoinegrondin@gmail.com>
2014-03-10 18:27:26 -0400

    Add time to the catchall Open.

--- goluago.go
+++ goluago.go
@@ -5,10 +5,12 @@ import (
 	"github.com/Shopify/goluago/encoding/json"
 	"github.com/Shopify/goluago/regexp"
 	"github.com/Shopify/goluago/strings"
+	"github.com/Shopify/goluago/time"
 )
 
 func Open(l *lua.State) {
 	regexp.Open(l)
 	strings.Open(l)
 	json.Open(l)
+	time.Open(l)
 }
