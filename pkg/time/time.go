8aa725051a88901d06a9762a2d59add246a87283
Diana Devasia <diana.devasia@shopify.com>
2020-10-22 16:07:28 -0400

    Updated the methods to format by taking the layout and zone. Also adding time by duration.

--- pkg/time/time.go
+++ pkg/time/time.go
@@ -16,24 +16,38 @@ func Open(l *lua.State) {
 
 var timeLibrary = []lua.RegistryFunction{
 	{"now", now},
-	{"nowUTC", nowUTC},
+	{"format", format},
 	{"sleep", sleep},
 	{"since", since},
-	{"addTime", addTime}
+	{"add", add},
 }
 
-func nowUTC(l *State) int {
-	l.PushString(time.Now().UTC().Format(time.RFC3339))
-	return 1
-}},
+func format(l *lua.State) int {
+	epochNanoToFormat := int64(lua.CheckNumber(l, 1))
+	layout := lua.CheckString(l, 2)
+	zone := lua.CheckString(l, 3)
 
-{"addHourUTC", func(l *State) int{
-	start := lua.CheckInteger(l, 1)
-	hourToAdd := lua.CheckInteger(l, 2)
-	diff := start.Add(time.Hour * time.Duration(hourToAdd))
-	l.PushString(diff.Format(time.RFC3339))
+	loc, err := time.LoadLocation(zone)
+	if err != nil {
+		lua.Errorf(l, err.Error())
+	}
+
+	unixTime := time.Unix(0, epochNanoToFormat)
+	timeInTimeZone := unixTime.In(loc)
+	l.PushString(timeInTimeZone.Format(layout))
+
 	return 1
-}},
+}
+
+func add(l *lua.State) int {
+	start := int64(lua.CheckNumber(l, 1))
+	startUnix := time.Unix(0, start)
+	valueToAdd := lua.CheckMap(l, 2)
+
+	inc := startUnix.Add((time.Hour * time.Duration(valueToAdd["hour"])) + (time.Minute * time.Duration(valueToAdd["minute"])) + (time.Second * time.Duration(valueToAdd["second"])))
+	l.PushNumber(float64(inc.UnixNano()))
+	return 1
+}
 
 func sleep(l *lua.State) int {
 	ns := lua.CheckInteger(l, 1)
