d8b9ad0db889b8e85d1433e158a2330735228645
Diana Devasia <diana.devasia@shopify.com>
2020-10-23 09:56:28 -0400

    Added time test for format and add

--- tst/time/time_test.lua
+++ tst/time/time_test.lua
@@ -13,3 +13,11 @@ local start = time.now()
 time.sleep(tenMicroSec)
 local diff = time.since(start)
 istrue("time: time since sleep must be bigger than sleep duration", diff > tenMicroSec)
+
+now := 1.257894e+18
+formattedTime := time.format(now, "2008-10-04T23:00:00Z")
+isEqual("time: format should be equal",  "2009-11-10T23:00:00Z", formattedTime)
+
+oneHrAdded := time.add(now, {"hour": 1})
+formattedOneHrLater := time.format(oneHrAdded, "2008-10-04T23:00:00Z")
+isEqual("time: format should be equal", "2009-11-11T00:00:00Z", formattedOneHrLater)
