6be78771af53eea273e871acef75634bbd394f51
Pier-Luc Caron-St-Pierre <pierluc.caronstpierre@shopify.com>
2021-06-18 10:57:31 -0400

    Add url safe base64 encoding

--- tst/encoding/base64/base64_test.lua
+++ tst/encoding/base64/base64_test.lua
@@ -7,3 +7,29 @@ equals("encodes a string to base64", "Zm9vYmFy", base64.encode("foobar"))
 --decode
 equals("an empty string outputs nothing", "", base64.decode(""))
 equals("decodes a base64 encded string", "ZXhhbXBsZS1tZXNzYWdlLWZvb2Jhcg==", base64.encode("example-message-foobar"))
+
+local value, err = pcall(function ()
+  return base64.decode("invalid base64")
+end)
+
+istrue("#decode returns error if the value is not a base64.", err)
+isfalse("#decode returns nil value is the value is not a base64.", value)
+
+equals(
+  "urlEncode",
+  "aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8_dGVzdD10cnVl",
+  base64.urlEncode("https://www.google.com/?test=true")
+)
+
+equals(
+  "urlDecode",
+  "https://www.google.com/?test=true",
+  base64.urlDecode("aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8_dGVzdD10cnVl")
+)
+
+local value, err = pcall(function ()
+  return base64.urlDecode("invalid base64")
+end)
+
+istrue("#urlDecode returns error if the value is not a base64.", err)
+isfalse("#urlDecode returns nil value is the value is not a base64.", value)
